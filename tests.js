253c34e98f43f756ace6dd357327bbd13ecf1a6e
ufumes <sms@ufumes.com>
2016-10-15 20:41:57 +0100

    completed sum_of_primes.js function

--- tests.js
+++ tests.js
@@ -3,22 +3,15 @@
 var chai = require('chai');
 var assert = chai.assert;
 
-var lib = require('./lib/library.js');
+var lib = require('./lib/sum_of_primes.js');
 
 describe("Test that constants are computed properly", function() {
   it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(2, 5), { a: 10, n: 4 })
-    );
+    assert(lib.sumOfprimes(5)==10, 'I am an error');
   });
-  it("should give 2, 1 for constants 1, 2 as a and n respectively", function() {
+   it("should give 10, 4 for constants 2, 5 as a and n respectively", function() {
     assert(
-      lib.compareCoefficients(lib.quadraticDerivative(1, 2), { a: 2, n: 1 })
-    );
+      lib.sumOfprimes(5)==10);
   });
-  it("should give 8, 1 for constants 4, 2 as a and n respectively", function() {
-    assert(
-      lib.compareCoefficients(lib.quadraticDerivative(4, 2), { a: 8, n: 1 })
-    );
-  });
+  
 });
